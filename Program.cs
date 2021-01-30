d2e1ae69a19c0a7ec6734af3c76c4b76a7ea91c9
bosinupebi <47193697+bosinupebi@users.noreply.github.com>
2021-01-30 15:20:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var driver = new ChromeDriver();
+           
+              
+                driver.Navigate().GoToUrl("URL");
+                driver.FindElement(By.Name("Username")).SendKeys("username");
+                driver.FindElement(By.Name("Password")).SendKeys("password");
+                driver.FindElement(By.Name("Password")).Submit();
+                System.Threading.Thread.Sleep(5000);
+                driver.FindElement(By.ClassName("mwLogout")).Click();
+              
+       
+    
+
+
+
+
+
+
+        }
+    }
+}
